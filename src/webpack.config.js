6c39cb97448c090545383d436a71887bae5e6c65
Leo <prodopark15@gmail.com>
2025-02-28 18:58:10 +0900

    chore: refactor webpack configuration

--- src/webpack.config.js
+++ src/webpack.config.js
@@ -1,11 +1,12 @@
 const path = require("path");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
 const MODE = process.env.WEBPACK_ENV;
 const ENTRY_FILE = path.resolve(__dirname, "assets", "js", "main.js");
 const OUTPUT_DIR = path.join(__dirname, "static");
 
 module.exports = {
-  entry: ["core-js/stable", "regenerator-runtime/runtime", ENTRY_FILE],
+  entry: ENTRY_FILE,
   mode: MODE,
   output: {
     filename: "main.js",
@@ -15,27 +16,46 @@ module.exports = {
     rules: [
       {
         test: /\.js$/,
-        exclude: "/node_modules",
-        use: ["babel-loader"],
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            cacheDirectory: true,
+          }
+        }
       },
       {
         test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, "css-loader"],
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader",
+            options: {
+              sourceMap: MODE === "development"
+            }
+          }
+        ]
       },
       {
-        test: /\.(jpe?g|png|gif|svg)$/,
-        loader: "file-loader",
-        options: {
-          name: "[name].[ext]",
-        },
-      },
-    ],
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        type: "asset/resource",
+        generator: {
+          filename: "images/[name][ext]"
+        }
+      }
+    ]
   },
   plugins: [
     new MiniCssExtractPlugin({
-      filename: "styles.css",
-    }),
+      filename: "styles.css"
+    })
   ],
-  devtool: "source-map",
+  devtool: MODE === "development" ? "eval-source-map" : "source-map",
   target: ["web", "es5"],
+  optimization: {
+    moduleIds: "deterministic",
+    splitChunks: {
+      chunks: "all"
+    }
+  }
 };
