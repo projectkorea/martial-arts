3943abaa3b9f84e8bf5b3bc18d58ab0063a0ada3
projectkorea <prodopark15@gmail.com>
2025-03-05 20:12:39 +0900

    chore: update dependencies and simplify Mongoose connection

--- src/db.js
+++ src/db.js
@@ -3,11 +3,7 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-mongoose.connect(process.env.MONGO_URL, {
-  useNewUrlParser: true,
-  useFindAndModify: false,
-  useUnifiedTopology: true,
-});
+mongoose.connect(process.env.MONGO_URL);
 
 const db = mongoose.connection;
 
