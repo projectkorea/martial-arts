a167e7b94e02b6178c5b1299220a5752a03a9f6e
projectkorea <prodopark15@gmail.com>
2025-03-11 23:13:04 +0900

    refactor: simplify home page and start script with random MBTI result functionality

--- src/assets/js/start.js
+++ src/assets/js/start.js
@@ -1,10 +1,37 @@
-const btnStart = document.querySelector(".start-btn"),
-  startPage = document.querySelector(".start-page");
+const startPage = document.querySelector(".start-page"),
+  btnStart = document.querySelector(".start-btn"),
+  randomBtn = document.querySelector(".random");
+
+function getRandomMBTIPath() {
+  const mbtiTypes = [
+  "ISTJ",
+  "ISFJ",
+  "INFJ",
+  "INTJ",
+  "ISTP",
+  "ISFP",
+  "INFP",
+  "INTP",
+  "ESTP",
+  "ESFP",
+  "ENFP",
+  "ENTP",
+  "ESTJ",
+  "ESFJ",
+  "ENFJ",
+  "ENTJ",
+];
+  const randomIndex = Math.floor(Math.random() * mbtiTypes.length);
+  return "/result/" + mbtiTypes[randomIndex];
+}
 
 const init = () => {
   btnStart.onclick = () => {
     location.href = "/question";
   };
+  randomBtn.onclick = () => {
+    location.href = getRandomMBTIPath();
+  };  
 };
 
 if (startPage) {
