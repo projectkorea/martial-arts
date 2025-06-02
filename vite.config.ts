22796946d43a8a4aad03700eb19bce8d5633d2f0
projectkorea <prodopark15@gmail.com>
2025-06-02 22:17:57 +0900

    refactor: vite

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,9 @@ import react from '@vitejs/plugin-react-swc'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig(({ mode }) => {
-  const base = mode === 'production' && process.env.BASE_URL ? process.env.BASE_URL : '/'
+  const isProduction = mode === 'production'
+  const base = isProduction ? '/static/mbti' : '/'
+  const outDir = isProduction ? `../../public/${base}` : './build'
   
   return {
     plugins: [
@@ -21,19 +23,10 @@ export default defineConfig(({ mode }) => {
       port: 3000
     },
     build: {
-      outDir: '../build/client',
-      // outDir: "../../public/static/mbti",
+      outDir,
       emptyOutDir: true,
       sourcemap: true,
-      assetsDir: 'assets',
-      rollupOptions: {
-        output: {
-          manualChunks: {
-            vendor: ['react', 'react-dom', 'react-router-dom', '@tanstack/react-query', '@tanstack/react-query-devtools'],
-          },
-          assetFileNames: 'assets/[name]-[hash][extname]'
-        }
-      }
+      assetsDir: 'assets'
     },
   }
 })
